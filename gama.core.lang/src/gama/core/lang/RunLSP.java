cdd3e28d20ef139b5ece89360d67421f1c75f10a
ndgnuh <ndgnuh@protonmail.com>
2021-06-29 14:41:11 +0700

    Using ServerLauncher as main class

---
+++ gama.core.lang/src/gama/core/lang/RunLSP.java
@@ -0,0 +1,12 @@
+package gama.core.lang;
+
+import org.eclipse.xtext.ide.server.ServerLauncher;
+
+
+public class RunLSP {
+
+	public static void main(String[] args) {
+		ServerLauncher.main(args);
+	}
+
+}
